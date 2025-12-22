2be231ca2c6906fe9ea94cbf9d53dedb4a2926cf
Alex <maralessio12@gmail.com>
2025-12-22 15:38:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for their age and convert it to an integer
+age = int(input("How old are you? "))
+
+# Print a message showing the user's age next year
+print(f"Hi {name}, in one year you will be {age + 1} years old")
